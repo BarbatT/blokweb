2451873faa44d0de0af63df7aeef51fdba8380d8
BarbatT <70375283+BarbatT@users.noreply.github.com>
2020-10-25 17:45:03 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 var slideIndex = 0;
-carousel();
-
+carousel ();
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("myBlog");
+  var x = document.getElementsByClassName ("myBlog");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
